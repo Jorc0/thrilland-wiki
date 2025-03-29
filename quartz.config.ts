41e6e25ff0affcf809a47c4ca483d56228e36b17
Jorc0 <jorc001@outlook.es>
2025-03-29 16:43:26 +0100

    Actualiza la configuración de Quartz: cambia el título de la página a "THRILLAND WIKI" y actualiza la URL base a "jorc0.github.io/thrilland-wiki".

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "BPL",
+    pageTitle: "THRILLAND WIKI",
     pageTitleSuffix: "v1",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "es-ES",
-    baseUrl: "jorc0.github.io/quartz",
+    baseUrl: "jorc0.github.io/thrilland-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
